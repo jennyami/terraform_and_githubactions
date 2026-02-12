f3610c3a4f5badf1708cde9e780daa80dff53a35
jennyami <jennilyn.ami@foodpanda.com>
2026-02-12 16:26:23 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
-  default     = "eu-north-1"
+  default     = "ap-southeast-1"
 }
 
 variable "default_tags" {
@@ -13,6 +13,6 @@ variable "default_tags" {
 
 variable "sqs_accident_queue_name" {
   type        = string
-  default     = "z-dh-gmlp-sqs-eu-north-1-accident-alerts"
+  default     = "z-dh-sqs-ap-southeast-1-accident-alerts"
   description = "The name of the SQS queue for alerts related to a device accident or unexpected event."
 }
