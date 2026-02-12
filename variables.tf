fb42f29fdd2d9d47d8ae5e5817296f486ea5239e
jennyami <jennilyn.ami@foodpanda.com>
2026-02-12 17:35:51 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,6 +8,12 @@ variable "default_tags" {
   type = map(string)
   default = {
     project_id = "1001"
+    dh_platform = "platform"
+    dh_tribe = "finance-systems"
+    dh_app = "github-terraform"
+    dh_squad = "fs-integration"
+    dh_cc_id = "1001010035"
+
   }
 }
 
