dd4bad09b9641ccb18e233377f48c0fed53edc87
jennyami <108661237+jennyami@users.noreply.github.com>
2026-03-31 14:00:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,3 +43,8 @@ resource "aws_sqs_queue" "new_queue2" {
   name = "z-dh-sqs-ap-southeast-1-new_queue2"
   tags = var.default_tags
 }
+
+resource "aws_sqs_queue" "new_queue3" {
+  name = "z-dh-sqs-ap-southeast-1-new_queue2"
+  tags = var.default_tags
+}
